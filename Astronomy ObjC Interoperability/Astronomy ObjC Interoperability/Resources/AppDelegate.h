6a748db3db7a8dc58c6643d5c10349ca383bd73d
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-03-11 16:55:43 -0500

    Project skeleton created and organized

---
+++ Astronomy ObjC Interoperability/Astronomy ObjC Interoperability/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Astronomy ObjC Interoperability
+//
+//  Created by Audrey Welch on 3/11/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Astronomy ObjC Interoperability/Astronomy ObjC Interoperability/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Astronomy ObjC Interoperability
+//
+//  Created by Audrey Welch on 3/11/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
